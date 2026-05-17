9f3498a0f7c0e70b70486a42b55ab557e293e14a
LCoderCraft <alfaroluis300705@gmail.com>
2026-05-16 19:19:33 -0700

    debug: enable APP_DEBUG para ver error real

--- docker-start.sh
+++ docker-start.sh
@@ -6,6 +6,10 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
+touch /var/www/html/storage/logs/laravel.log
+chmod 666 /var/www/html/storage/logs/laravel.log
+
 php-fpm --daemonize
 
+tail -f /var/www/html/storage/logs/laravel.log &
 nginx -g "daemon off;"
