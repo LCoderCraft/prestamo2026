de6b25ef6de4734a4dc9b8f6b32539185f0e3395
LCoderCraft <alfaroluis300705@gmail.com>
2026-05-17 15:46:32 -0700

    otro commit

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -10,14 +10,22 @@ class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        User::firstOrCreate(
-            ['email' => 'prestamodematerialesuas@gmail.com'],
-            [
-                'username' => 'admin',
-                'phone' => '6671234567',
-                'role' => 'admin',
-                'password' => Hash::make('admin123'),
-            ]
-        );
+        $admins = [
+            ['username' => 'LUIS',   'email' => 'prestamodematerialesuas+luis@gmail.com'],
+            ['username' => 'ANGEL',  'email' => 'prestamodematerialesuas+angel@gmail.com'],
+            ['username' => 'Leticia','email' => 'prestamodematerialesuas+leticia@gmail.com'],
+        ];
+
+        foreach ($admins as $admin) {
+            User::firstOrCreate(
+                ['email' => $admin['email']],
+                [
+                    'username' => $admin['username'],
+                    'phone'    => '6671234567',
+                    'role'     => 'admin',
+                    'password' => Hash::make('admin123'),
+                ]
+            );
+        }
     }
 }
